e9c8a784d77fd8cc731264dcedff3a408787d3cf
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 02:25:24 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -28,6 +28,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.any? {|i| cheese_types.include?(i) }
+    new = array & cheese_types
     
   else
     return nil
