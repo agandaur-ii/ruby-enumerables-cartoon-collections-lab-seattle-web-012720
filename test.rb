2ad2fec5b0e85cf26849101728a9b3bd9795e3e7
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 02:13:04 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -19,3 +19,9 @@ def long_planeteer_calls(array)
   array.any? { |word| word.length > 4 }
 end
 
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.any? {|i| cheese_types.include?(i) }
+end
+
