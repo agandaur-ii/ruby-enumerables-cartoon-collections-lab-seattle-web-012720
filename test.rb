bee6146855dbf9ff2ad79f06018a5f3c93a4031b
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 01:27:44 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 dwarves = ["Dopey", "Grumpy", "Bashful"]
 
 def roll_call_dwarves(array)
-  array.each_with_index {|name, index| puts "#{index}: #{name}"}
+  array.each_with_index {|name, index| puts "#{index + 1}: #{name}"}
 end
