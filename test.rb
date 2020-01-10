f301ad5183c85fca2b890a05bc90f7dc08269506
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-10 01:40:06 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -4,4 +4,8 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}: #{name}"}
 end
 
+def summon_captain_planet(array)
+  array.map(&capitalize)
+end
+
 puts roll_call_dwarves(dwarves)
